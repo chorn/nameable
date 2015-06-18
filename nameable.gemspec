3eccb1dbec804ce1131c80da88cba98248e501cc
chorn <chorn@chorn.com>
2015-06-18 07:26:47 -0400

    Add codeclimate settings

--- nameable.gemspec
+++ nameable.gemspec
@@ -21,4 +21,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency 'bundler', '>= 1.6.2'
   spec.add_development_dependency 'rake'
   spec.add_development_dependency 'rspec', '~> 3.0.0'
+  spec.add_development_dependency 'codeclimate-test-reporter'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,10 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require "bundler/setup"
 Bundler.require(:test)
+
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
+
 require "nameable"
 
 RSpec.configure do |config|
