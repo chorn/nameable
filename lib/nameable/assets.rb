08f4dbab475f02c7331859e44d07f8c76db5a714
chorn <chorn@chorn.com>
2017-06-16 07:21:47 -0400

    Update first name -> gender file

--- lib/nameable/assets.rb
+++ lib/nameable/assets.rb
@@ -1,4 +1,4 @@
 module Nameable::Assets
-  GENDER_TABLE    = File.expand_path(File.join('..', '..', '..', 'data', 'yob2014.txt'), __FILE__)
+  GENDER_TABLE    = File.expand_path(File.join('..', '..', '..', 'data', 'yob2016.txt'), __FILE__)
   ETHNICITY_TABLE = File.expand_path(File.join('..', '..', '..', 'data', 'app_c.csv'), __FILE__)
 end
